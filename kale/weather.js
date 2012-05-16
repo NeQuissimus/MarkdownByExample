3d8dedc7e8206fddf6b884d5c981e2809444b216
Tim Steinbach <steinbach.tim@googlemail.com>
2012-05-15 21:42:44 -0400

    Remove unnecessary variable from JS

--- kale/weather.js
+++ kale/weather.js
@@ -1,3 +1,2 @@
 var query = escape('select item from weather.forecast where location="GMXX0035" and u="c"');
 var url = "http://query.yahooapis.com/v1/public/yql?format=json&q=" + query;
-var temp = "N/A";
